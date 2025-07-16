94f70356beeb948c39ecdc19a0c66bbac5175333
unknown <behzadimohammadmahdi55@gmail.com>
2025-07-16 04:26:18 -0700

    class EquipName

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DormitoryManagement
+{
+    public enum EquipName
+    {
+        Refrigerator = 1,
+        Table = 2,
+        Chair = 3,
+        Bed = 4,
+        Closet = 5
+    }
+    public enum Status
+    {
+        Healthy = 1,
+        Faulty = 2,
+        UnderRepair = 3
+    }
+    public class Equipment
+    {
+        public EquipName Type {  get; set; }
+        public int EquipmentId { get; set; }
+        public Status Status { get; set; }
+        public int RoomNumber { get; set; }
+        public string AssignedStudentNationalCode { get; set; }
+        public Equipment(EquipName type, int equipmentId, Status status, int roomNumber, string studentNationalCode)
+        {
+            Type = type;
+            EquipmentId = equipmentId;
+            Status = status;
+            RoomNumber = roomNumber;
+            AssignedStudentNationalCode = studentNationalCode;
+        }
+    }
+}
